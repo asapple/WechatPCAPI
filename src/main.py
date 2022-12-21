ff65f83e17fbf90d10653601d055c66a63101602
asapple <960099622@qq.com>
2022-12-21 17:50:41 +0800

    1. 重命名文件

--- src/main.py
+++ src/main.py
@@ -3,20 +3,54 @@
 # @Author  : Leon
 # @Email   : 1446684220@qq.com
 # @File    : test.py
-# @Desc    : 
+# @Desc    :
 # @Software: PyCharm
 
+import os
 from WechatPCAPI import WechatPCAPI
 import time
-
+wx_inst = None
 
 # 接收消息的回调函数，可自行定义
+
+
 def on_message(message):
     print(message)
+    if isNeededInfo(message) and isAbnormalInfo(message):
+        handleFunc(message)
+    return
+
+# 判断此消息是否需要留意
+
+
+def isNeededInfo(message):
+    if 'data' in message and 'from_chatroom_wxid' in message['data']:
+        return 1
+    else:
+        return 0
+
+# 判断此消息是否是炸群信息
+
+
+def isAbnormalInfo(message):
+    if message['data']['msg'] == '他们是托':
+        return 1
+    else:
+        return 0
+
+# 异常信息出现时的处理函数
+
+
+def handleFunc(message):
+    global wx_inst
+    wx_inst.send_text(to_user='filehelper', msg=message['data']['time'] +
+                      ':' + message['data']['from_chatroom_nickname'] + message['data']['msg'])
+    return
 
 
 def main():
     # 初始化wx实例
+    global wx_inst
     wx_inst = WechatPCAPI(on_message=on_message)
 
     # 启动微信 目前仅支持微信V2.7.1.82
@@ -28,21 +62,6 @@ def main():
 
     # 登录成功了
     print(wx_inst.get_myself())
-
-    # 以下尝试发送各类消息给文件传输助手，可以换成任何人的wx_id
-    wx_inst.send_text(to_user='filehelper', msg='777888999')
-    wx_inst.send_link_card(
-        to_user='filehelper',
-        title='我的博客',
-        desc='我的博客，红领巾技术分享网站',
-        target_url='http://www.honglingjin.online/',
-        img_url=''
-    )
-    wx_inst.send_img(to_user='filehelper', img_abspath=r'C:\Users\Leon\Pictures\1.jpg')
-    wx_inst.send_file(to_user='filehelper', file_abspath=r'C:\Users\Leon\Desktop\1.txt')
-    wx_inst.send_gif(to_user='filehelper', gif_abspath=r'C:\Users\Leon\Desktop\08.gif')
-    wx_inst.send_card(to_user='filehelper', wx_id=wx_inst.wx_id)
-
     time.sleep(10)
     # 更新所有好友信息，数据会通过上面的回调函数返回
     wx_inst.update_frinds()
